c44767b40f8abff8eebbd8c73cc31fa488d83475
Agent Smith <agent.smith@blueprint41.com>
2023-10-16 16:30:59 +0800

    initial commit

---
+++ MyBlueprint41App1/MyBlueprint41App1.Model/Datastore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Blueprint41;
+
+namespace MyBlueprint41App1.Model
+{
+    public class Datastore : DatastoreModel<Datastore>
+    {
+        [Version(1, 0, 0)]
+        protected void Initial()
+        {
+            // Define your data model here            
+            throw new NotImplementedException();
+        }
+
+        protected override void SubscribeEventHandlers()
+        {
+            // Susbcribe your event handlers here
+            throw new NotImplementedException();
+        }
+    }
+}
